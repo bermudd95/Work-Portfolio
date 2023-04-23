6cfdbb4dadbe917d7559c69528dcbfe96d5e11eb
Danny Bermudez <danny@Dannys-MacBook-Pro.local>
2023-04-23 13:03:54 -0700

    new projects added

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -3,170 +3,281 @@ import React from 'react';
 
 const Work = () => {
   return (
-		<div className='w-full md:h-screen'>
+		<div className='w-full md:h-full'>
 			<div className='max-w-[1000px] mx-auto p-4 flex-col justify-center w-full h-full'>
 				<div className='pb-8 text-center'>
 					<h2 className='text-3xl font-bold'>Projects</h2>
-          <h2 className='py-6'> Check out my most recent work.</h2>
+					<h2 className='py-6'> Check out my most recent work.</h2>
 				</div>
 				<div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          
-          {/* Grid Items */}
-          <div
-            style={{
-              backgroundImage:
-                "url(https://repository-images.githubusercontent.com/556975408/9289575e-42f9-4d24-bdc7-068e22df5ad0)",
-            }} 
-            className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'>
-          
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                YouView
-              </span>
-						  <div>
-							  <a target='_blank' rel='noreferrer' href='https://bermudd95.github.io/Youview-app/'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-							  </a>
-                <a target='_blank' rel='noreferrer' href='https://github.com/bermudd95/Youview-app'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-							  </a>
-						  </div>
-            </div>
+					<div
+						style={{
+							backgroundImage:
+								"url(https://repository-images.githubusercontent.com/631439095/42073932-1ae0-4cd1-a1c0-10c71a3ee60e)",
+						}}
+						className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'
+					>
+						<div className='opacity-0 group-hover:opacity-100'>
+							<span className='text-2xl font-bold text-white tracking-wider'>
+								Desktop Productivity App
+							</span>
+							<div>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://productivity-app-iota.vercel.app/'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Demo
+									</button>
+								</a>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://github.com/bermudd95/productivity-app'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
 					</div>
-          <div
-            style={{
-              backgroundImage:
-                "url(https://repository-images.githubusercontent.com/548468894/85795baf-83b9-4690-a870-6b844b8771aa)",
-            }} 
-            className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'>
-          
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Weather-app
-              </span>
-						  <div>
-							  <a target='_blank' rel='noreferrer' href='https://bermudd95.github.io/Weather-App/'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-							  </a>
-                <a target='_blank' rel='noreferrer' href='https://github.com/bermudd95/Weather-App/'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-							  </a>
-						  </div>
-            </div>
+					<div
+						style={{
+							backgroundImage:
+								"url(https://repository-images.githubusercontent.com/625403659/3a9bed46-be1b-421e-88bf-a528a80607ac)",
+						}}
+						className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'
+					>
+						<div className='opacity-0 group-hover:opacity-100'>
+							<span className='text-2xl font-bold text-black tracking-wider'>
+								Mock Store Dashboard
+							</span>
+							<div>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://ap-stats.vercel.app/'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Demo
+									</button>
+								</a>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://github.com/bermudd95/AP-Stats'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
 					</div>
-          <div
-            style={{
-              backgroundImage:
-                "url(https://repository-images.githubusercontent.com/545208464/32753419-8338-4ce9-848c-3e9ff240b05e)",
-            }} 
-            className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'>
-          
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Movies.app
-              </span>
-						  <div>
-							  <a target='_blank' rel='noreferrer' href='https://bermudd95.github.io/Movie-app/'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-							  </a>
-                <a target='_blank' rel='noreferrer' href='https://github.com/bermudd95/Movie-app'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-							  </a>
-						  </div>
-            </div>
+					<div
+						style={{
+							backgroundImage:
+								"url(https://repository-images.githubusercontent.com/556975408/9289575e-42f9-4d24-bdc7-068e22df5ad0)",
+						}}
+						className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'
+					>
+						<div className='opacity-0 group-hover:opacity-100'>
+							<span className='text-2xl font-bold text-white tracking-wider'>
+								YouView
+							</span>
+							<div>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://bermudd95.github.io/Youview-app/'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Demo
+									</button>
+								</a>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://github.com/bermudd95/Youview-app'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
 					</div>
-          <div
-            style={{
-              backgroundImage:
-                "url(https://repository-images.githubusercontent.com/619047607/c16107b9-2259-467b-9893-7b865ef11f85)",
-            }} 
-            className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'>
-          
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-black tracking-wider'>
-                Cryptocurrency.io
-              </span>
-						  <div>
-							  <a target='_blank' rel='noreferrer' href='https://cryptocurrency-io.vercel.app'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-							  </a>
-                <a target='_blank' rel='noreferrer' href='https://github.com/bermudd95/cryptocurrency.io'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-							  </a>
-						  </div>
-            </div>
+					<div
+						style={{
+							backgroundImage:
+								"url(https://repository-images.githubusercontent.com/548468894/85795baf-83b9-4690-a870-6b844b8771aa)",
+						}}
+						className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'
+					>
+						{/* Hover Effects */}
+						<div className='opacity-0 group-hover:opacity-100'>
+							<span className='text-2xl font-bold text-white tracking-wider'>
+								Weather-app
+							</span>
+							<div>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://bermudd95.github.io/Weather-App/'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Demo
+									</button>
+								</a>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://github.com/bermudd95/Weather-App/'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
 					</div>
-          <div
-            style={{
-              backgroundImage:
-                "url(https://repository-images.githubusercontent.com/599757987/6ac36e55-9cb9-444e-aaca-e98a2368e5bf)",
-            }} 
-            className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'>
-          
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Netflix 2.0
-              </span>
-						  <div>
-							  <a target='_blank' rel='noreferrer' href='https://netflix-clone-six-virid.vercel.app/'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-							  </a>
-                <a target='_blank' rel='noreferrer' href='https://github.com/bermudd95/Netflix-clone'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-							  </a>
-						  </div>
-            </div>
+					<div
+						style={{
+							backgroundImage:
+								"url(https://repository-images.githubusercontent.com/545208464/32753419-8338-4ce9-848c-3e9ff240b05e)",
+						}}
+						className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'
+					>
+						{/* Hover Effects */}
+						<div className='opacity-0 group-hover:opacity-100'>
+							<span className='text-2xl font-bold text-white tracking-wider'>
+								Movies.app
+							</span>
+							<div>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://bermudd95.github.io/Movie-app/'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Demo
+									</button>
+								</a>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://github.com/bermudd95/Movie-app'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
 					</div>
-          <div
-            style={{
-              backgroundImage:
-                "url(https://repository-images.githubusercontent.com/594595791/4e1cb7fd-7c1a-444e-8073-080d73a28bdb)",
-            }} 
-            className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'>
-          
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Makeup Studio
-              </span>
-						  <div>
-							  <a target='_blank' rel='noreferrer' href='https://makeup-studio.vercel.app/'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-							  </a>
-                <a target='_blank' rel='noreferrer' href='https://github.com/bermudd95/Makeup-studio'>
-								  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-							  </a>
-						  </div>
-            </div>
+					<div
+						style={{
+							backgroundImage:
+								"url(https://repository-images.githubusercontent.com/619047607/c16107b9-2259-467b-9893-7b865ef11f85)",
+						}}
+						className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'
+					>
+						{/* Hover Effects */}
+						<div className='opacity-0 group-hover:opacity-100'>
+							<span className='text-2xl font-bold text-white tracking-wider'>
+								Cryptocurrency.io
+							</span>
+							<div>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://cryptocurrency-io.vercel.app'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Demo
+									</button>
+								</a>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://github.com/bermudd95/cryptocurrency.io'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
+					</div>
+					<div
+						style={{
+							backgroundImage:
+								"url(https://repository-images.githubusercontent.com/599757987/6ac36e55-9cb9-444e-aaca-e98a2368e5bf)",
+						}}
+						className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'
+					>
+						{/* Hover Effects */}
+						<div className='opacity-0 group-hover:opacity-100'>
+							<span className='text-2xl font-bold text-white tracking-wider'>
+								Netflix 2.0
+							</span>
+							<div>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://netflix-clone-six-virid.vercel.app/'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Demo
+									</button>
+								</a>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://github.com/bermudd95/Netflix-clone'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
+					</div>
+					<div
+						style={{
+							backgroundImage:
+								"url(https://repository-images.githubusercontent.com/594595791/4e1cb7fd-7c1a-444e-8073-080d73a28bdb)",
+						}}
+						className='shadow-lg group container rounded-md flex justify-center items-center bg-cover bg-no-repeat mx-auto content-div'
+					>
+						{/* Hover Effects */}
+						<div className='opacity-0 group-hover:opacity-100'>
+							<span className='text-2xl font-bold text-white tracking-wider'>
+								Makeup Studio
+							</span>
+							<div>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://makeup-studio.vercel.app/'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Demo
+									</button>
+								</a>
+								<a
+									target='_blank'
+									rel='noreferrer'
+									href='https://github.com/bermudd95/Makeup-studio'
+								>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
