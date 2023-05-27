b4ab2aa3beb23d3a5003c495327d0def2ea6d10a
Danny Bermudez <danny@Dannys-MacBook-Pro.local>
2023-05-26 18:49:12 -0700

    navbar fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 		<div
 			className={`w-screen h-full flex flex-wrap justify-between items-center px-4 md: bg-slate-400`}
 		>
-			<div className='w-screen flex justify-between md:block'>
+			<div className='w-screen flex justify-between md:block py-3'>
 				<h1 className='text-4xl md:underline font-bold md:text-center text-white'>
 					Danny Bermudez
 				</h1>
@@ -23,27 +23,27 @@ const Navbar = () => {
 					isOpen ? "block" : "hidden"
 				}`}
 			>
-				<li className='md:pr-3 mt-4 md:mt-0 md:text-xl md:text-center md:w-full cursor-pointer hover:text-gray-200 hover:border-x-2 hover:border-black'>
+				<li className='md:pr-3 mt-4 md:mt-0 md:text-xl md:text-center md:w-full cursor-pointer hover:text-gray-200 md:hover:border-x-2 md:hover:border-black'>
 					<Link to='home' smooth={true} duration={500}>
 						Home
 					</Link>
 				</li>
-				<li className='md:pr-3 mt-4 md:mt-0  md:text-xl md:text-center md:w-full cursor-pointer hover:text-gray-200 hover:border-x-2 hover:border-black'>
+				<li className='md:pr-3 mt-4 md:mt-0  md:text-xl md:text-center md:w-full cursor-pointer hover:text-gray-200 md:hover:border-x-2 md:hover:border-black'>
 					<Link to='about' smooth={true} duration={500}>
 						About
 					</Link>
 				</li>
-				<li className='md:pr-3 mt-4 md:mt-0  md:text-xl md:text-center md:w-full cursor-pointer hover:text-gray-200 hover:border-x-2 hover:border-black'>
+				<li className='md:pr-3 mt-4 md:mt-0  md:text-xl md:text-center md:w-full cursor-pointer hover:text-gray-200 md:hover:border-x-2 md:hover:border-black'>
 					<Link to='skills' smooth={true} duration={500}>
 						Skills
 					</Link>
 				</li>
-				<li className='md:pr-3 mt-4 md:mt-0  md:text-xl md:text-center md:w-full cursor-pointer hover:text-gray-200 hover:border-x-2 hover:border-black'>
+				<li className='md:pr-3 mt-4 md:mt-0  md:text-xl md:text-center md:w-full cursor-pointer hover:text-gray-200 md:hover:border-x-2 md:hover:border-black'>
 					<Link to='work' smooth={true} duration={500}>
 						Work
 					</Link>
 				</li>
-				<li className='md:pr-3 my-4 md:my-0  md:text-xl md:text-center md:w-full cursor-pointer hover:text-gray-200 hover:border-x-2 hover:border-black'>
+				<li className='md:pr-3 my-4 md:my-0  md:text-xl md:text-center md:w-full cursor-pointer hover:text-gray-200 md:hover:border-x-2 md:hover:border-black'>
 					<Link to='contact' smooth={true} duration={500}>
 						Contact
 					</Link>
